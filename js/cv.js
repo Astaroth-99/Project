1aac604cd7eaca4bb6d49ed64b457725653f88c0
Astaroth- <88864348+Astaroth-99@users.noreply.github.com>
2023-02-28 22:04:11 -0300

    Add files via upload

---
+++ js/cv.js
@@ -0,0 +1,9 @@
+function redireccion()
+{
+    location.href ="https://www.argentina.gob.ar/economia/conocimiento/argentina-programa"
+}
+        
+function redireccion2()
+{
+    ocation.href ="https://es-la.facebook.com/"
+}
